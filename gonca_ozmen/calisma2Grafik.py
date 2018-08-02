b691ef3e6f1f1e04f8635eb88da4d6d566a621df
goncaOzmen <41535539+goncaOzmen@users.noreply.github.com>
2018-08-02 16:08:17 +0300

    grafik düzeltildi

--- gonca_ozmen/calisma2Grafik.py
+++ gonca_ozmen/calisma2Grafik.py
@@ -16,7 +16,7 @@ sweb_var=[0, 48.2, 63.1, 62.4, 58.7, 52.5, 55.4, 58.0, 53.8, 56.6, 65.5, 66.0, 7
 
 bKullanim= [23.6, 22.9, 33.4, 38.0, 40.1, 43.2, 46.4, 48.7, 49.9, 53.5, 54.8, 54.9, 56.6]
 bKullanim_male= [31.1, 30.0, 42.7, 47.8, 50.5, 53.4, 56.1, 59.0, 60.2, 62.7, 64.0, 64.1, 65.7]
-bKullanim_female=[16.2, 15,9,23.7, 28.5, 30.0, 33.2, 36.9, 38.5, 39.8, 44.3, 45.6, 45.9, 47.7]
+bKullanim_female=[16.2, 15.9,23.7, 28.5, 30.0, 33.2, 36.9, 38.5, 39.8, 44.3, 45.6, 45.9, 47.7]
 intKullanim=[18.8, 17.6, 30.1, 35.9, 38.1, 41.6,45.0, 47.4, 48.9, 53.8, 55.9, 61.2,66.8]
 intKul_male=[25.7, 24.0, 39.2, 45.4, 48.6, 51.8, 54.9, 58.1, 59.3, 63.5, 65.8, 70.5, 75.1]
 intKul_female=[12.1, 11.1, 20.7, 26.6, 28.0, 31.7, 35.3, 37.0, 38.7, 44.1, 46.1, 51.9, 58.7]
@@ -25,27 +25,53 @@ intKullanim=[18.8, 17.6, 30.1, 35.9, 38.1, 41.6,45.0, 47.4, 48.9, 53.8, 55.9, 61
 
 internetErisim=[7.0, 8.7, 19.7, 25.4, 30.0, 41.6, 42.9, 47.2, 49.1, 60.2, 69.5, 76.3, 80.7]
 
-eni= 0.9
+eni= 0.35
 
 
 
 figure, ax1 = plt.subplots()
 
-ax1.bar(y_pos,kkullanim)
-ax1.bar(y_pos +eni, sint_erisim)
+
+opacity = 0.4
+error_config = {'ecolor': '0.3'}
+
+rects1 = ax1.bar(y_pos, kkullanim, eni,
+                alpha=opacity, color='red',
+                error_kw=error_config, label="Kurumsal Kullanim")
 
 
-plt.title(' Bilgisayar kullanim')
+
+rects2 = ax1.bar(y_pos+eni, sint_erisim, eni,
+                alpha=opacity, color='blue',
+                error_kw=error_config,
+                label='Kurumsal erişim')
+
+c3=ax1.plot(y_pos,sweb_var,label="Kurumsal Web Sayfası")
+#p3=ax2.plot(y_pos,sweb_var)plt.savefig("grafik_Bilgi2.png")
+
+ax1.set_xlabel('Yıllar')
+ax1.set_ylabel('%')
+ax1.set_title('Yıllara Göre Kurumsal Bilgisayar Kullanımı')
+ax1.set_xticks(y_pos + eni/ 2)
+ax1.set_xticklabels(objects)
+ax1.legend()
+plt.xticks(rotation=90)
+
+# figure.tight_layout()
+plt.show()
+
+plt.savefig("grafik_Bilgi3.png")
+
 
 
 # plt.legend((kkullanim,sint_erisim[1]),('Kullanim', 'Erisim'))
 
-plt.xticks(y_pos + eni , objects)
-# plt.legend(loc='best')
-plt.xticks(rotation=90)
-plt.show()
+# plt.xticks(y_pos , objects)
+# # plt.legend(loc='best')
+# plt.xticks(rotation=90)
+# plt.show()
 
-plt.savefig("grafik_Bilgi.png")
+
 
 
 
